a3b9f6faf6c2d90b9ccaa342ef810368fa5375e2
akashshetty737 <47499504+akashshetty737@users.noreply.github.com>
2019-02-10 19:30:38 +0530

    Update dev_predict.py

--- dev_predict.py
+++ dev_predict.py
@@ -1,20 +1,19 @@
-import scipy
-from scipy.misc import imread
-from scipy.misc import imresize
+from scipy.misc import imread, imresize #not needed
 import numpy as np
-x = imread('data/dev/test/0/9512.png', mode='L')
+import imageio
+from PIL import Image
+x = imageio.imread('test3.png',pilmode='L')
 #compute a bit-wise inversion so black becomes white and vice versa
 x = np.invert(x)
 #make it the right size
-x = imresize(x,(32,32))
+x =np.array(Image.fromarray(x).resize([28,28]))
 #convert to a 4D tensor to feed into our model
-x = x.reshape(1,32,32,1)
+x = x.reshape(1,28,28,1)
 x = x.astype('float32')
 x /= 255
 
 #perform the prediction
-import keras
 from keras.models import load_model
-model = load_model('data/dev_modelV1.h5')
+model = load_model('cnn.h5')
 out = model.predict(x)
 print(np.argmax(out))
