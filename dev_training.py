da571748060ce631345b92983213465517b369e3
akashshetty737 <47499504+akashshetty737@users.noreply.github.com>
2019-02-10 21:49:44 +0530

    Update dev_training.py

--- dev_training.py
+++ dev_training.py
@@ -14,8 +14,8 @@ from keras.layers import Conv2D, MaxPooling2D
 from keras import backend as K
 
 # Providing dataset path
-train_data_path = 'data/dev/train'
-validation_data_path = 'data/dev/validation'
+train_data_path = 'dev/train'
+validation_data_path = 'dev/validation'
 
 # Proving train samples
 train_samples = 20000
@@ -27,13 +27,13 @@ epoch = 4
 
 # model begins here
 
-img_width, img_height = 32, 32
+img_width, img_height = 28,28
 
 dev_model = Sequential()
 dev_model.add(Conv2D(32, kernel_size=(3,3),
                  activation='relu',
-                 input_shape=(img_width, img_height, 3)))
-# change here at input shape as 32,32,1
+                 input_shape=(img_width, img_height, 1)))
+# change here at input shape as 28,28,1
 dev_model.add(Conv2D(64, (3, 3), activation='relu'))
 dev_model.add(MaxPooling2D(pool_size=(2, 2)))
 dev_model.add(Dropout(0.25))
@@ -61,6 +61,7 @@ test_datagen = ImageDataGenerator(rescale=1./255)
 train_generator = train_datagen.flow_from_directory(
     train_data_path,
     target_size=(img_width, img_height),
+    color_mode="grayscale",
     batch_size=32,
     class_mode='categorical'
 )
@@ -68,12 +69,13 @@ train_generator = train_datagen.flow_from_directory(
 validation_generator = test_datagen.flow_from_directory(
     validation_data_path,
     target_size=(img_width, img_height),
+    color_mode="grayscale",
     batch_size=32,
     class_mode='categorical')
 
 dev_model.fit_generator(
     train_generator,
-    samples_per_epoch = train_samples,
+    steps_per_epoch = train_samples,
     nb_epoch = epoch,
     validation_data = validation_generator,
     nb_val_samples = validation_samples
